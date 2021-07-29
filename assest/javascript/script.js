c9a0ffcaf1fffc7639bdfe62c5288b9a0b978472
khaled-963-etmah <khaled.312.etmah@gmail.com>
2021-07-29 13:05:54 +0300

    Initial commit

---
+++ assest/javascript/script.js
@@ -0,0 +1,19 @@
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(150, function() {
+        $(this).remove();
+    })
+    event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        var inputtext = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class=\"fas fa-trash-alt\"></i></span> " + inputtext + "</li>");
+    }
+});
+$(".fa-plus").click(function() {
+    $("input[type='text']").fadeToggle();
+});
